f56c0a561f0f3abf45b5cb898f5d212112d0d039
GDG Buenos Aires <jonatan@gdg.com.ar>
2015-02-23 20:45:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 echo "Levantando la instancia de nodejs con nohup"
-nohup nodejs server.js --fe_ip PUT_YOUR_PRIVATE_IP_HERE &
+nohup nodejs server.js --fe_ip 10.240.149.196 &
 echo "La instancia se levanto correctamente"
